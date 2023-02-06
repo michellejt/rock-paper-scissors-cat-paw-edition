6369df9729dde777caeab0ef1558b60da1901b23
michellejt <mtongas@gmail.com>
2023-02-06 22:57:20 +1030

    logic

--- app.js
+++ app.js
@@ -6,39 +6,69 @@ const userChoice = [
     ["paper", "paper.png"],
     ["scissors", "scissors.png"] 
 ] 
-const computerChoice = document.querySelector('#computer-choice')
-const computerChoiceDisplay = document.getElementById('computer-choice')
+const playComputerChoice = document.getElementById('computer-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice-display')
+const resultDisplay = document.getElementById('result')
+let userChoiceSelected 
+let result
+let i = 0;
 
- let i = 0;
- choose.addEventListener('click', e => {
+choose.addEventListener('click', e => {
 
   if ( i < userChoice.length - 1) { i++; } else { i = 0;} 
-    var userChoiceImage = "<img src='images/" + userChoice[i][1] + "'>"; 
-    console.log("my choice is " + userChoice[i][0]);
-    console.log(userChoiceImage, e)
-    //userChoiceDisplayImage.innerHTML = userChoiceImage;
+    var userChoiceImage = "<img src='images/" + userChoice[i][1] + "'>";
+    userChoiceSelected = userChoice[i][0]; 
+    console.log(userChoiceSelected);
+    //console.log(userChoiceImage, e)
     userChoiceDisplayImage.innerHTML =  userChoiceImage
-    //userChoiceDisplay.innerHTML = userChoice[i];
-    //console.log(userChoice[i])
+    userChoiceDisplay.innerHTML = userChoiceSelected;
 }) 
 
-computerChoice.addEventListener('click', e => { 
-  /* Create a variable that will hold  the text and when inserted into HTML document it will be rendered as an HTML img element */
- var repImage = '<img src="'; 
- const randomNumber = Math.floor(Math.random() * 3) +1
- console.log(randomNumber)
- if (randomNumber === 1){
-     userChoice ="rock"
-     repImage += 'rock.png">'; 
- }
- if (randomNumber === 2){
-     userChoice ="scissors"
-     repImage += 'scissors.png">'; 
- }
- if (randomNumber === 3){
-     userChoice ="paper"
-     repImage += 'paper.png">'; 
- }
- userChoiceDisplayImage.innerHTML = repImage;
- userChoiceDisplay.innerHTML = userChoice;
-})
+  playComputerChoice.addEventListener('click', (e) => {
+    // userChoice = e.target.id
+    // userChoiceDisplay.innerHTML = userChoice
+     generateComputerChoice()
+     getResult()
+   })
+   
+   function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    
+    if (randomNumber === 1) {
+      computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+      computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+      computerChoice = 'paper'
+    }
+    console.log("computer choice" + computerChoice)
+    computerChoiceDisplay.innerHTML = computerChoice
+  }
+  
+  function getResult() {
+    console.log('get result ' + userChoiceSelected)
+    if (computerChoice === userChoiceSelected) {
+      result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoiceSelected === "paper") {
+      result = 'you win!'
+    }
+    if (computerChoice === 'rock' && userChoiceSelected === "scissors") {
+      result = 'you lost!'
+    }
+    if (computerChoice === 'paper' && userChoiceSelected === "scissors") {
+      result = 'you win!'
+    }
+    if (computerChoice === 'paper' && userChoiceSelected === "rock") {
+      result = 'you lose!'
+    }
+    if (computerChoice === 'scissors' && userChoiceSelected === "rock") {
+      result = 'you win!'
+    }
+    if (computerChoice === 'scissors' && userChoiceSelected === "paper") {
+      result = 'you lose!'
+    }
+    resultDisplay.innerHTML = result
+  }
